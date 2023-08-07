a03a0ab5b26f9c43399290dae664516f271b1d76
Park <akp6031@e5-cse-205-12.cse.psu.edu>
2023-08-07 00:32:56 -0400

    More bitvector tests

--- tests/test_bitvector.cpp
+++ tests/test_bitvector.cpp
@@ -49,7 +49,65 @@ void test_bitvector_ranks(){
     assert(~found);
 }
 
+void test_fixed_size_bitvector(){
+    bm::bvector<> bv(100);
+    bv.set_bit(10, true);
+    std::unique_ptr<bm::bvector<>::rs_index_type>
+                                rs_idx(new bm::bvector<>::rs_index_type());
+    bv.build_rs_index(rs_idx.get());
+
+    assert(bv.rank(9, *rs_idx)==false);
+    assert(bv.rank(10, *rs_idx)==true);
+    assert(bv.rank(11, *rs_idx)==true);
+}
+
+void test_large_size_bitvector(){
+    uint64_t N = 80000000000;
+    uint64_t test_count = 10000;
+    bm::bvector<> bv(N);
+    vector<int> numbers;
+    bool print = false;
+
+    auto start = std::chrono::steady_clock::now();
+    for (int i = 0; i < test_count; i++){
+        int no = rand() % N;
+        if(no == 0){
+            continue;
+        }
+        numbers.push_back(no);
+    }
+
+    if(print) std::cout << "sampling(ms)=" << (std::chrono::steady_clock::now()-start).count()/1000000 << std::endl;
+    start = std::chrono::steady_clock::now();
+
+    for(auto no: numbers){
+        bv.set_bit(no);
+    }
+
+    if(print) std::cout << "set bits(ms)=" << (std::chrono::steady_clock::now()-start).count()/1000000 << std::endl;
+    start = std::chrono::steady_clock::now();
+
+    std::unique_ptr<bm::bvector<>::rs_index_type>
+                                rs_idx(new bm::bvector<>::rs_index_type());
+    bv.build_rs_index(rs_idx.get());
+
+    if(print) std::cout << "indexing(ms)=" << (std::chrono::steady_clock::now()-start).count()/1000000 << std::endl;
+    start = std::chrono::steady_clock::now();
+
+    for (auto no: numbers){
+        assert(bv.rank(no-1, *rs_idx)<bv.rank(no, *rs_idx));
+    }
+
+    if(print) std::cout << "check(ms)=" << (std::chrono::steady_clock::now()-start).count()/1000000 << std::endl;
+    
+    // assert(bv.rank(size-3, *rs_idx)==false);
+    // assert(bv.rank(size-2, *rs_idx)==true);
+    // assert(bv.rank(size-1, *rs_idx)==true);
+}
+
 void main_bitvector() {
     cout << "test_ranks finished" << endl; 
     test_bitvector_ranks();
+    test_fixed_size_bitvector();
+    test_large_size_bitvector();
 }
