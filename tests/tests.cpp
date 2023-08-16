8d66c4b45342cc395ca125883dfb37dd17cbfa9d
Park <akp6031@e5-cse-205-12.cse.psu.edu>
2023-08-16 18:36:12 -0400

    Rename files

--- tests/tests.cpp
+++ tests/tests.cpp
@@ -1,18 +1,18 @@
 #include "test_fm_index.cpp"
 #include "test_naive_fm_index.cpp"
 #include "test_bitvector.cpp"
-#include "test_construction.repr.cpp"
+#include "test_construction.repr.annot.cpp"
 #include "test_construction.maxrep.cpp"
 #include "test_construction.navigation.cpp"
 // #include "test_construction.mincover.cpp"
-#include "test_construction.repr_annot.cpp"
+#include "test_construction.repr.rule.cpp"
 
 int main(void){
     cout << "--- main_fm_index ---" << endl;
     main_fm_index();
     cout << "--- main_naive_fm_index ---" << endl;
     main_naive_fm_index();
-    // cout << "--- main_fm_index ---" << endl;
+    // cout << "--- main_mincover ---" << endl;
     // main_construction_mc();
     cout << "--- main_construction_navigation ---" << endl;
     main_construction_navigation();
