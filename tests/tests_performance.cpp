981a5fe09380fa6a5299732a73e482e7f896798b
Park <akp6031@e5-cse-205-12.cse.psu.edu>
2023-08-30 00:30:45 -0400

    Add tree performance tests

--- tests/tests_performance.cpp
+++ tests/tests_performance.cpp
@@ -1,9 +1,12 @@
 #include "test_performance.bitvector.cpp"
 #include "test_performance.construction.cpp"
+#include "test_performance.tree.cpp"
 
 int main(void){
-    cout << "--- bitvector ---" << endl;
-    main_performance_bitvector();
-    cout << "--- construction ---" << endl;
-    main_performance_construction();
+    // cout << "--- bitvector ---" << endl;
+    // main_performance_bitvector();
+    // cout << "--- construction ---" << endl;
+    // main_performance_construction();
+    cout << "--- tree ---" << endl;
+    main_performance_tree();
 }
