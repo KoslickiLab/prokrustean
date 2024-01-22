a2bed11be06215127d8c917dc492b60b40bea151
Park <akp6031@e5-cse-205-12.cse.psu.edu>
2024-01-22 17:10:53 -0500

    Support fastq extension

--- scripts/get_bwt_grl.py
+++ scripts/get_bwt_grl.py
@@ -24,24 +24,41 @@ make
 def main(input):
     # input file fastq name ex. "./downloads/x_x_x.fastq.gz"
     in_path_for_fastq_file = input
+    
+    if str.endswith(input, '.fastq.gz'):
+        extension='.fastq.gz'
+    elif str.endswith(input, '.fastq'):
+        extension='.fastq'
+    else:
+        raise Exception('non-fastq file (fastq.gz or .fastq)')
+
     # input file becomes concatenated ex. "./downloads/x_x_x.txt"
-    in_path_for_concatenated_string = in_path_for_fastq_file.replace(".fastq.gz", ".txt")
+    in_path_for_concatenated_string = in_path_for_fastq_file.replace(extension, ".txt")
     # grlbwt processed. they don't like underscore ex. "./downloads/xxx.txt"
-    out_path_grlbwt_rl_bwt_file = in_path_for_fastq_file.replace(".fastq.gz", "").replace("_", "")
+    out_path_grlbwt_rl_bwt_file = in_path_for_fastq_file.replace(extension, "").replace("_", "")
     # grlbwt processed to text. ex. "./downloads/x_x_x.bwt"
-    out_txt_path_grlbwt_txt_file = in_path_for_fastq_file.replace(".fastq.gz", ".bwt")
+    out_txt_path_grlbwt_txt_file = in_path_for_fastq_file.replace(extension, ".bwt")
     print(f'input: {input}')
     print(f'sequences will be concatenated : {in_path_for_concatenated_string}')
     print(f'grlbwt will be processed : {out_path_grlbwt_rl_bwt_file}')
     print(f'grlbwt output will be converted to txt : {out_txt_path_grlbwt_txt_file}')
 
-    def process_fastq_gz(file_path, output_file_path):
+    def process_fastq_gz(file_path, output_file_path, extension):
         concatenated_sequence = ""
-        with gzip.open(file_path, "rt", encoding="utf-8") as file:  # 'rt' stands for read text
-            lines = file.readlines()
-            for i, line in enumerate(lines):
-                if (i % 4 == 1):  # sequences are located every 4 lines starting at line 1 (0-indexed)
-                    concatenated_sequence += line.strip() + '$'
+        if extension=='.fastq.gz':
+            with gzip.open(file_path, "rt", encoding="utf-8") as file:  # 'rt' stands for read text
+                lines = file.readlines()
+                for i, line in enumerate(lines):
+                    if (i % 4 == 1):  # sequences are located every 4 lines starting at line 1 (0-indexed)
+                        concatenated_sequence += line.strip() + '$'
+        elif extension=='.fastq':
+            with open(file_path, "rt") as file:  # 'rt' stands for read text
+                lines = file.readlines()
+                for i, line in enumerate(lines):
+                    if (i % 4 == 1):  # sequences are located every 4 lines starting at line 1 (0-indexed)
+                        concatenated_sequence += line.strip() + '$'
+        else:
+            raise Exception('non-fastq extension')
         with open(output_file_path, 'w') as file:
             file.write(concatenated_sequence)
 
@@ -50,7 +67,7 @@ def main(input):
         os.system(f'../../grlbwt/build/grl2plain {out_file_path}.rl_bwt {out_txt_file_path}')
 
     # preprocess 
-    process_fastq_gz(in_path_for_fastq_file, in_path_for_concatenated_string)
+    process_fastq_gz(in_path_for_fastq_file, in_path_for_concatenated_string, extension)
 
     # grlbwt and conversion to txt 
     run_grlbwt(in_path_for_concatenated_string, out_path_grlbwt_rl_bwt_file, out_txt_path_grlbwt_txt_file)
